daba02f383c5eec3bad5c33ea0ef4fbde476e0df
Soumik Mukherjee <170843649+deadMartial@users.noreply.github.com>
2024-11-28 13:44:47 +0530

    Half done

--- javascript.js
+++ javascript.js
@@ -2,10 +2,10 @@ function add(a,b) {
     return parseInt(a)+parseInt(b);
 }
 function subtract(a,b) {
-    return parseInt(a)+parseInt(b);
+    return parseInt(a)-parseInt(b);
 }
 function multiply(a,b) {
-    return parseInt(a)+parseInt(b);
+    return parseInt(a)*parseInt(b);
 }
 function divide(a,b) {
     return parseFloat(a)/parseFloat(b);
@@ -15,6 +15,12 @@ let op1=0;
 let op2=0;
 let op="";
 
+const btns=document.querySelectorAll(".ops");
+btns.forEach((btn)=> {
+    btn.addEventListener("click", printScreen);
+})
+
+const sre=document.querySelector(".display");
 
 function operate(op,op1,op2) {
     switch(op) {
@@ -24,3 +30,54 @@ function operate(op,op1,op2) {
         case "/":return divide(op1,op2);
     }
 }
+
+function printScreen(e) {
+    if(e.target.classList.contains("num")) {
+        printScreenNum(e);
+    }
+    else if(e.target.classList.contains("util")) {
+        printScreenUtil(e);
+    }
+    else if (e.target.classList.contains("op")) {
+        printScreenOp(e);
+    }console.log(op," ",op1," ",op2)
+}
+
+
+function printScreenNum(e) {
+
+    console.log(e.target);
+    if(e.target.textContent=="." && sre.textContent.includes(".")){return}
+    sre.textContent+=e.target.textContent;
+    
+}
+
+function printScreenUtil(e) {
+    if(e.target.textContent==="AC") {
+        sre.textContent="";
+    }
+    else if(e.target.textContent==="=") {
+        op2=parseInt(sre.textContent);
+        sre.textContent=operate(op,op1,op2);
+        op="";
+        op2=0;
+        op1=parseInt(sre.textContent)
+    }
+}
+
+function printScreenOp(e) {
+    if(sre.textContent==="") {
+        op1=0;return;
+    }
+    else if(op!=="") {
+        sre.textContent=operate(op,op2,op2);
+
+    }
+    
+        op1=parseInt(sre.textContent);
+        op=e.target.textContent;
+        sre.textContent="";
+    
+
+}
+
